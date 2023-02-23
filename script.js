cc10db9d69fa8795dc8eeb5bd6abe83b8b80c5d9
Sabrina Contaldi <sabrinacontaldi_2023@depauw.edu>
2023-02-23 18:24:51 -0500

    ‘initial’

---
+++ script.js
@@ -0,0 +1,19 @@
+var hamburger = document.querySelector(".hamb");
+var navlist = document.querySelector(".nav-list");
+var links = document.querySelector(".nav-list li");
+
+hamburger.addEventListener("click", function(){
+	this.classList.toggle("click");
+	navlist.classList.toggle("open");
+
+});
+
+var github = document.function(".github")
+
+// window.open("https://github.com/sabrinacontaldi", Github, specs);
+
+git.addEventListener("click", function(){
+	this.classList.toggle("click");
+	navlist.classList.toggle("open");
+
+});
